f17badf2dbcdbb05d65dd923fbe4d58f4281f178
griminir <viktor.elliot.king@gmail.com>
2024-04-05 12:47:06 +0200

    Co-authored-by: camillahu <camillahu@users.noreply.github.com> Co-authored-by: Kulen94 <Kulen94@users.noreply.github.com> Co-authored-by: hawwokitty <hawwokitty@users.noreply.github.com>

---
+++ scripts/model.js
@@ -0,0 +1,21 @@
+model = {
+  app: {
+    isLoggedIn: false,
+    loggedInUser: null,
+    admin: false,
+    page: '',
+  },
+  input: {},
+  data: {
+    users: [
+      {
+        id: 0,
+        username: 'altofar',
+      },
+      {
+        id: 1,
+        username: 'bob',
+      },
+    ],
+  },
+};
