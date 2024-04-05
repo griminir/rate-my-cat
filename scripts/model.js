185f5a7ef92ea1f751ac1a67f55b3b1a09be9a28
griminir <viktor.elliot.king@gmail.com>
2024-04-05 13:28:12 +0200

    Co-authored-by: camillahu <camillahu@users.noreply.github.com> Co-authored-by: Kulen94 <Kulen94@users.noreply.github.com> Co-authored-by: hawwokitty <hawwokitty@users.noreply.github.com> Co-authored-by: Ellie Marie <Dretvik@users.noreply.github.com>

--- scripts/model.js
+++ scripts/model.js
@@ -5,17 +5,103 @@ model = {
     admin: false,
     page: '',
   },
-  input: {},
+  input: {
+    postCat: {
+      addNewCat: {
+        id: null,
+        owner: model.app.loggedInUser,
+        name: '',
+        age: null,
+        dateOfBirth: undefined,
+        race: [],
+        lore: ``,
+        pics: [],
+      },
+      addMoreCatPics: {
+        pics: [],
+      },
+    },
+    userRegistration: {
+      username: '',
+      password: '',
+      disclaimer: false,
+    },
+    logInInfo: {
+      logInUsername: '',
+      logInPassword: '',
+    },
+    editProfile: {
+      bio: '',
+      profilePic: '',
+    },
+    viewCat: {
+      rating: null,
+      comment: {
+        commenter: model.app.loggedInUser,
+        comment: '',
+      },
+    },
+    reportIssue: {
+      reportee: model.app.loggedInUser,
+      reportedUser: null,
+      reportReason: '',
+    },
+  },
   data: {
     users: [
       {
-        id: 0,
         username: 'altofar',
+        password: 'password',
+        bio: '',
+        profilePic: '',
       },
       {
-        id: 1,
         username: 'bob',
+        password: 'bob123',
+        bio: '',
+        profilePic: '',
       },
+    ],
+    cats: [
+      {
+        id: 0,
+        owner: model.data.users.username,
+        name: 'Oliver',
+        age: 12,
+        dateOfBirth: undefined,
+        race: [model.data.catRaces[0]],
+        lore: `Han ble arrestert for skattesvindel og heleri i 2017, 
+                er kretsmester i Mario Kart 64 og har litt for høyrevridde meninger 
+                rundt nåværende norsk innvandringspolitikk`,
+        rating: 10,
+        comments: [
+          {
+            commenter: model.data.users.username,
+            comment: 'I love ur cat',
+          },
+          {
+            commenter: model.data.users.username,
+            comment: 'I hate your cat grr',
+          },
+        ],
+      },
+    ],
+    catRaces: [
+      'British Shorthair',
+      'Sphynx',
+      'Toyger',
+      'Highlander',
+      'Himalayan',
+      'Egyptian Mau',
+      'Savannah',
+      'Scottish Fold',
+      'Siamese',
+      'Bengal',
+      'Persian',
+      'Maine Coon',
+      'Ragdoll',
+      'Norwegian Forest',
+      'Unknown',
     ],
   },
 };
