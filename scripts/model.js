cb167daee0dee20ed996c9a84987c636ab9176af
camillahu <camillahaugenurdal@gmail.com>
2024-04-10 14:49:43 +0200

    model
    lagt til min bruker og loki i model.data

--- scripts/model.js
+++ scripts/model.js
@@ -3,7 +3,7 @@ let model = {
     isLoggedIn: false,
     loggedInUser: 'aalto',
     admin: false,
-    page: '',
+    page: 'feed',
   },
   input: {
     postCat: {
@@ -68,13 +68,22 @@ let model = {
       {
         username: 'altofar',
         password: 'password',
-        bio: 'test test test',
+        bio: `test test test`,
         profilePic: '',
       },
       {
         username: 'bob',
         password: 'bob123',
-        bio: '',
+        bio: ``,
+        profilePic: '',
+      },
+      {
+        username: 'camillzy',
+        password: 'camilla',
+        bio: `Dale a tu cuerpo alegría Macarena
+              Que tu cuerpo es pa' darle alegría y cosa buena
+              Dale a tu cuerpo alegría, Macarena
+              Hey Macarena, ay`,
         profilePic: '',
       },
     ],
@@ -90,7 +99,7 @@ let model = {
                 er kretsmester i Mario Kart 64 og har litt for høyrevridde meninger 
                 rundt nåværende norsk innvandringspolitikk`,
         rating: 10,
-        pics: ['img/catPictures/oliver1.png', 'img/catPictures/loki1.jpg'],
+        pics: ['img/catPictures/oliver1.png'],
         comments: [
           {
             commenter: 'henrik', //model.data.users.username
@@ -112,6 +121,28 @@ let model = {
         lore: `Previously owned by norwegian mafia`,
         rating: 10,
         pics: ['img/catPictures/bianca1.jpg'],
+        comments: [
+          {
+            commenter: 'henrik', //model.data.users.username
+            comment: 'I love ur cat',
+          },
+          {
+            commenter: 'bob', // model.data.users.username
+            comment: 'I hate your cat grr',
+          },
+        ],
+      },
+      {
+        id: 2,
+        owner: 'camillzy', // model.data.users.username
+        name: 'Loki',
+        age: 6,
+        dateOfBirth: null,
+        race: [], // model.data.catraces[0]
+        lore: `Hun kaster opp jenvlig fordi hun har sensitiv tarm, men er veldig glad i sokkelo.
+              Hun terroriserer oss om natta, men på dagtid er hun bare søt. `,
+        rating: 8.5,
+        pics: ['img/catPictures/loki1.jpg', 'img/catPictures/loki2.jpg'],
         comments: [
           {
             commenter: 'henrik', //model.data.users.username
