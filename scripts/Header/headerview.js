0013c555e7d1f5c343f4372f231fb98f033fc6bd
griminir <viktor.elliot.king@gmail.com>
2024-04-11 12:45:43 +0200

    Update headerview.js

--- scripts/Header/headerview.js
+++ scripts/Header/headerview.js
@@ -1,6 +1,18 @@
 function makeHeader() {
   let html = /*html*/ `
-    <header><div>hello</div></header>
+    <header>
+      <img class="H-logo" src="img/RMP_transparent_color_border.png" alt="logo" onclick="changePage('feed')" />
+      <nav class="H-nav">
+        <ul class="H-nav-links">
+          <li class="H-li"><a href="#">${
+            model.app.loggedInUser ?? 'profile'
+          }</a></li>
+          <li class="H-li"><a href="#">${
+            model.app.loggedInUser ?? 'profile'
+          }</a></li>
+        </ul>
+      </nav>
+    </header>
   `;
 
   return html;
