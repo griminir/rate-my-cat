d12b9c5009b5f521a632c57b435ff1fbf7f14585
griminir <viktor.elliot.king@gmail.com>
2024-04-11 14:32:49 +0200

    header temp done
    cssing for header

--- scripts/Header/headerview.js
+++ scripts/Header/headerview.js
@@ -1,19 +1,17 @@
 function makeHeader() {
   let html = /*html*/ `
-    <header>
+    <header class="H-header">
       <img class="H-logo" src="img/RMP_transparent_color_border.png" alt="logo" onclick="changePage('feed')" />
       <nav class="H-nav">
         <ul class="H-nav-links">
           <li class="H-li"><a class="H-a" onclick="loggedInCheck1()" href="#">${
-            model.app.loggedInUser ?? 'profile'
+            model.app.loggedInUser ?? 'Profile'
           }</a></li>
-          <li class="H-li H-2-1"><a class="H-a" href="#">Post My Pussy</a>
-            <div onclick="loggedInCheck2()" class="H-option"> Add Pussy</div>
-            <div onclick="loggedInCheck3()" class="H-option"> Edit Pussy</div> 
-          </li>
+          <li class="H-li"><a onclick="loggedInCheck2()" class="H-a" href="#">Add Pussy</a></li>
+          <li class="H-li"><a onclick='loggedInCheck3()' class="H-a" href="#">Edit Pussy</a></li>
         </ul>
       </nav>
-      <button class="H-Button">Login</button>
+      <button class="H-button">Login</button>
     </header>
   `;
 
