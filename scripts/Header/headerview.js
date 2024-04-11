4f562fe18ef7f9cfbc3cfb811e460d5e9ea87323
griminir <viktor.elliot.king@gmail.com>
2024-04-11 13:30:45 +0200

    description
    added css and linked it

--- scripts/Header/headerview.js
+++ scripts/Header/headerview.js
@@ -4,15 +4,16 @@ function makeHeader() {
       <img class="H-logo" src="img/RMP_transparent_color_border.png" alt="logo" onclick="changePage('feed')" />
       <nav class="H-nav">
         <ul class="H-nav-links">
-          <li class="H-li"><a onclick="loggedInCheck1()" href="#">${
+          <li class="H-li"><a class="H-a" onclick="loggedInCheck1()" href="#">${
             model.app.loggedInUser ?? 'profile'
           }</a></li>
-          <li class="H-li H-2-1"><a href="#">Post My Pussy</a>
+          <li class="H-li H-2-1"><a class="H-a" href="#">Post My Pussy</a>
             <div onclick="loggedInCheck2()" class="H-option"> Add Pussy</div>
             <div onclick="loggedInCheck3()" class="H-option"> Edit Pussy</div> 
           </li>
         </ul>
       </nav>
+      <button class="H-Button">Login</button>
     </header>
   `;
 
