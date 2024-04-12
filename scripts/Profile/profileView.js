c061008be51708d6ba905c7fbcbbdf995dc13e62
griminir <viktor.elliot.king@gmail.com>
2024-04-12 14:29:21 +0200

    fixet noe code
    jeez

--- scripts/Profile/profileView.js
+++ scripts/Profile/profileView.js
@@ -1,22 +1,11 @@
 function profileView() {
   let div = document.getElementById('app');
   let html = /*html*/ `
-  <section class="profile"
-  <div class="username">
-  <h2 class="bio">
-  
-
-  
-
-
-   
-
-
-
-<button class="editButton" onclick="edit()" >edit</button>
-
-
-  
-
+  <section class="profile">
+    <div class="username">name here</div>
+    <h2 class="bio">bio here</h2>
+    <button class="editButton" onclick="edit()" >edit</button>
+  </section>
 `;
+  div.innerHTML = html;
 }
