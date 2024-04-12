444eb93ebd4407900aa5721b17bd6001d89b657a
Kulen94 <157685291+Kulen94@users.noreply.github.com>
2024-04-12 14:15:32 +0200

    profileview
    fått inn litt div osv

--- scripts/Profile/profileView.js
+++ scripts/Profile/profileView.js
@@ -1,4 +1,23 @@
 function profileView() {
   let div = document.getElementById('app');
-  let html = /*html*/ ``;
+  let html = /*html*/``
+  
+  <section class="profile"
+  <div class="username">
+  <h2 class="bio">
+  
+
+  
+
+
+   
+
+
+
+<button class="editButton" onclick="edit()" >edit</button>
+
+
+  
+
+
 }
