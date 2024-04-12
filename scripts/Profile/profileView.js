d40d3c2bc5a62ecb534e0a3f286aeb2fe6056685
griminir <viktor.elliot.king@gmail.com>
2024-04-12 14:35:48 +0200

    Update profileView.js

--- scripts/Profile/profileView.js
+++ scripts/Profile/profileView.js
@@ -4,7 +4,7 @@ function profileView() {
   <section class="profile">
     <div class="username">name here</div>
     <h2 class="bio">bio here</h2>
-    <button class="editButton" onclick="edit()" >edit</button>
+    <button class="editButton" onclick="edit()">edit</button>
   </section>
 `;
   div.innerHTML = html;
